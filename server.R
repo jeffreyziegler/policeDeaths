84778c8857c1f0d46be73ddfcdb6013ce7749b50
zieglerjef <zieglerjef@gmail.com>
2016-03-13 14:12:40 -0500

    Add server.R

--- server.R
+++ server.R
@@ -11,15 +11,15 @@ map <- rename(map, replace=c("region"="state", "subregion"="county"))
 # capitalize entries
 map$county <- capitalize(map$county)
 map$state <- capitalize(map$state)
-# subset out AK and HI
+# subset out AK and HI (only continental U.S.)
 death <- subset(death,  ! state %in% c("HI", "AK"))
 # merge datasets
 map <- join(map, census, by = c("county", "state"))
 
-# shiny
+# shiny app
 shinyServer(
 function(input,output){
-  
+  # render plot output
   output$map <- renderPlot({
     
     # create input variables 
@@ -27,12 +27,26 @@ function(input,output){
                        "Black population (%)" = map$blackPopPercent,
                        "Median income ($)" = map$medianIncome)
     
-    # plot
-    ggplot() + geom_polygon(data = map, aes(x = long, y = lat, group = group, fill = vars)) +
+    # open ggplot
+    ggplot() +
+      # fill polygons based on latitude and longitude
+      geom_polygon(data = map, aes(x = long, y = lat, group = group, fill = vars)) +
+      # create points for each recorded death
       geom_point(data = death, aes(x = long, y = lat), color = "red", size = 2) +
-      theme(axis.line = element_blank(),axis.text.x=element_blank(), axis.text.y=element_blank(), axis.ticks=element_blank(), axis.title.x=element_blank(), axis.title.y=element_blank(), panel.background=element_blank(), panel.border=element_blank(), panel.grid.major=element_blank(), panel.grid.minor=element_blank(), plot.background=element_blank()) +
-      scale_fill_continuous(guide = guide_legend(title = " ", title.position = "left", label.position = "bottom"), low="grey80", high="grey10")
+      # alter theme
+      # no axis lines or text
+      theme(axis.line = element_blank(), axis.text.x=element_blank(),
+            axis.text.y=element_blank(), axis.ticks=element_blank(),
+            axis.title.x=element_blank(), axis.title.y=element_blank(),
+            # remove panel background
+            panel.background=element_blank(), panel.border=element_blank(),
+            panel.grid.major=element_blank(), panel.grid.minor=element_blank(),
+            plot.background=element_blank()) +
+      # generate scale fill to fill polygons
+      scale_fill_continuous(guide = guide_legend(title = " ",
+                            title.position = "left",label.position = "bottom"),
+                            low="grey80", high="grey10")
     
-    
+    # determine height of map output
   }, height = 550)
 })
